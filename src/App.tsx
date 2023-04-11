0e386f528e98b1d52334c59bc594adc66a56c744
Shreyam Maity <sm8967724231@gmail.com>
2023-04-11 18:02:35 +0530

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ export default function App() {
             group: "Admins",
             properties: {
                 name: {
-                    name: "Name",
+                    name: "name",
                     dataType: "string"
                 }
             }
